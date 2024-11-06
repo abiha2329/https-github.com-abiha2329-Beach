597882cd58f7c7b0d5c6f973da82da7cae8292d1
Abiha Rahman <arahm18@u.rochester.edu>
2024-11-06 14:35:36 -0500

    Initial Commit

---
+++ BeachMaster/BeachMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeachMasterApp.swift
+//  BeachMaster
+//
+//  Created by Abiha Rahman on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct BeachMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeachMaster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BeachMaster
+//
+//  Created by Abiha Rahman on 11/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
